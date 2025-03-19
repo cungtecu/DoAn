5bb4919b37b3f41edefd96712d11af712a59598a
cungtecu <103375251+cungtecu@users.noreply.github.com>
2025-03-19 14:21:54 +0700

    fix

--- app/src/main/java/com/example/doan/SplashActivity.java
+++ app/src/main/java/com/example/doan/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.example.doan;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,9 +14,23 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         // Delay 2 giây rồi mở MainActivity
-        new Handler().postDelayed(() -> {
-            startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            finish();
-        }, 2000);
+        VideoView videoView = findViewById(R.id.Intro);
+
+        // Đường dẫn video từ thư mục raw
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.intro;
+        Uri uri = Uri.parse(videoPath);
+
+        // Gán video vào VideoView
+        videoView.setVideoURI(uri);
+
+        // Bắt đầu phát video
+        videoView.start();
+
+        // Khi video kết thúc, chuyển sang MainActivity
+        videoView.setOnCompletionListener(mp -> {
+            Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+            startActivity(intent);
+            finish(); // Đóng màn hình loading
+        });
     }
 }
