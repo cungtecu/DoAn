3c9102b767ad803924490dbb5a21a6625bf39d4a
TuanAn <an033123@gmail.com>
2025-03-25 10:44:58 +0700

    update code 25/3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,13 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("androidx.viewpager2:viewpager2:1.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.daimajia.androidanimations:library:2.4@aar")
+}
+dependencies {
+    implementation("androidx.recyclerview:recyclerview:1.3.2") // Hoặc phiên bản mới nhất
+}
+dependencies {
+    implementation("com.google.android.material:material:1.12.0") // Thay <version> bằng phiên bản mới nhất, ví dụ: 1.6.0, 1.7.0, 1.8.0, 1.9.0
 }
